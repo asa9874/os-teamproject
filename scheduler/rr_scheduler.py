34ef717c740819cb4d01429fdd256e6c4dc68bcd
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2025-04-26 17:40:51 +0900

    Feat: RR 로직 구현

--- scheduler/rr_scheduler.py
+++ scheduler/rr_scheduler.py
@@ -1,11 +1,32 @@
 from scheduler import BaseScheduler
 
 class RRScheduler(BaseScheduler):
+    def __init__(self, processes, processors_info, time_quantum: int) -> None:
+        super().__init__(processes, processors_info)
+        self.time_quantum = time_quantum
+
+    # RRScheduler는 FCFS와 비슷하게 처리된다.
     def schedule(self):
-        pass
-        
+        for processor in self.processors_info:
+            if not processor.is_available():
+                processor.execute()
+                
+    # RRScheduler는 기본적으로 FCFS를 따르지만 할당량 초과 시 회수 및 할당이 이루어진다.
     def assign_process(self):
-        pass         
-    
+        for processor in self.processors_info:
+            # 실행 중이면 할당량 만료시 회수
+            if not processor.is_available() and self.current_time - processor.current_process.start_time >= self.time_quantum:
+                self.ready_queue.appendleft(processor.current_process)
+                processor.drop_process()
+            if processor.is_available():     # 비어있는 경우 할당
+                if self.ready_queue:
+                    process = self.ready_queue.pop() #FIFO 방식으로 프로세스 할당
+                    processor.assign_process(process,self.current_time)
+                    break
+
+    # RRScheduler는 time quantum을 초과하여 수행한 경우 선점이 일어난다.
     def ready_queue_update(self):
-        pass
+        for process in self.processes:
+            # 프로세스가 도착했지만 대기 큐에 없고 실행 중이지 않으며 남은 시간이 있는 경우
+            if process.arrival <= self.current_time and not process.is_running() and process not in self.ready_queue and process.remaining_time > 0:
+                self.ready_queue.appendleft(process)
