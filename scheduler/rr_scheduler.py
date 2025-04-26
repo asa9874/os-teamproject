33820c76dab88130e018470f42d53e605c3f2aea
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2025-04-26 19:02:35 +0900

    Feat: RR 로직에 충돌 고려하도록 수정

--- scheduler/rr_scheduler.py
+++ scheduler/rr_scheduler.py
@@ -1,3 +1,4 @@
+import heapq
 from scheduler import BaseScheduler
 
 class RRScheduler(BaseScheduler):
@@ -7,7 +8,7 @@ class RRScheduler(BaseScheduler):
             if not processor.is_available():
                 processor.execute()
                 
-    # RRScheduler는 FCFS처럼 할당하지만 할당량 만료 시 회수가 이루어진다.
+    # RRScheduler는 FCFS처럼 할당하지만 실행 중에 할당량 만료 시 회수가 이루어진다.
     def assign_process(self):
         for processor in self.processors_info:
             # 실행 중이면 할당량 만료시 회수
@@ -21,9 +22,14 @@ class RRScheduler(BaseScheduler):
                     processor.assign_process(process,self.current_time)
                     break
 
-    # RRScheduler는 time quantum을 초과하여 수행한 경우 선점이 일어난다.
+    # RRScheduler는 같은 시각에 여러 프로세스가 들어올 수 있어 충돌이 발생할 수 있다.
+    # 우선순위: pid가 높은 순
     def ready_queue_update(self):
+        process_priority_queue = []
         for process in self.processes:
             # 프로세스가 도착했지만 대기 큐에 없고 실행 중이지 않으며 남은 시간이 있는 경우
             if process.arrival <= self.current_time and not process.is_running() and process not in self.ready_queue and process.remaining_time > 0:
-                self.ready_queue.appendleft(process)
+                heapq.heappush(process_priority_queue, (-process.pid, process))
+        while process_priority_queue:
+            process = heapq.heappop(process_priority_queue)[1]
+            self.ready_queue.appendleft(process)
