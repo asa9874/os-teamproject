1b3b3f80ae0542070e2382ba3955f128179c599d
terazim00 <164006553+terazim00@users.noreply.github.com>
2025-05-01 23:36:06 +0900

    주석 추가
    코드의 설명에 관한 주석이 미흡한거 같아 보강 후 다시 commit했습니다

--- scheduler/srtn_scheduler.py
+++ scheduler/srtn_scheduler.py
@@ -2,7 +2,7 @@ from scheduler import BaseScheduler
 from collections import deque
 
 class SRTNScheduler(BaseScheduler):
-    def schedule(self): #SRTN스케줄링 선점방식으로 프로세서가 도착시 RT가 작은 프로세서를 실행 / 똑같을 시 AT기준으로 실행행
+    def schedule(self): #SRTN스케줄링 선점방식으로 프로세서가 도착시 RT가 작은 프로세서를 실행 / 똑같을 시 AT기준으로 실행
         for processor in self.processors_info:
             if not processor.is_available():
                 processor.execute(self.current_time)
@@ -11,14 +11,14 @@ class SRTNScheduler(BaseScheduler):
         for processor in self.processors_info:
             if not processor.is_available() and self.ready_queue:
                 shortest = min(self.ready_queue, key=lambda p: (p.remaining_time, p.arrival)) #RT를 기준으로 보조키 AT를 기준으로 정렬
-                if shortest.remaining_time < processor.current_process.remaining_time:
-                    self.ready_queue.remove(shortest)
-                    self.ready_queue.appendleft(processor.current_process)
+                if shortest.remaining_time < processor.current_process.remaining_time: #실행중인것과 큐에서 짧은 프로세스를 비교(false시 그대로 실행)
+                    self.ready_queue.remove(shortest) #큐에서 실행할 프로세서를 제거
+                    self.ready_queue.appendleft(processor.current_process) #큐에 실행중인 프로세서를 삽입
                     processor.drop_process()
-                    processor.assign_process(shortest, self.current_time)
+                    processor.assign_process(shortest, self.current_time) #실행
             if processor.is_available(): #프로세서에 비어있을 떄
-                if self.ready_queue: #RT를 기준으로 보조키 AT를 보조키로 정렬
-                    self.ready_queue = deque(sorted(self.ready_queue, key=lambda p: (p.remaining_time, p.arrival), reverse=True))
-                    next_proc = self.ready_queue.pop()
-                    processor.assign_process(next_proc, self.current_time)
+                if self.ready_queue:
+                    self.ready_queue = deque(sorted(self.ready_queue, key=lambda p: (p.remaining_time, p.arrival), reverse=True)) #RT를 기준으로 보조키 AT를 보조키로 정렬
+                    next_process = self.ready_queue.pop() #정렬된 큐에서 가장 RT가 짧은 프로세서를 next_process로 지정
+                    processor.assign_process(next_process, self.current_time) #실행
                     break
